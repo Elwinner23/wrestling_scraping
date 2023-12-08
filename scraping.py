f4ece81d25e4e544c7021c2bf4fe02ad9b080a3f
Kheyransa Pashayeva <xeyransa.pshayeva@gmail.com>
2023-12-08 16:27:52 +0400

    div-den textler list formatda yuklenir

--- scraping.py
+++ scraping.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from openpyxl import Workbook
 from selenium.webdriver.common.by import By
 import pandas as pd
+import time 
 
 driver = webdriver.Firefox()
 url = 'https://uww.org/events'
@@ -17,7 +18,7 @@ for li in select_list.find_elements(By.TAG_NAME, 'li'):
     button = li.find_element(By.TAG_NAME, 'button')
     if button.text in years:
         button.click()
-
+time.sleep(2)
 
 main_table = driver.find_element(By.CLASS_NAME,'table-responsive')
 table_wrapper = main_table.find_element(By.CLASS_NAME,'table-wrapper')
@@ -28,11 +29,8 @@ element_table = table_wrapper.find_element(By.XPATH,'/html/body/div/div/div/div/
 data = []
 wb = Workbook()
 ws = wb.active
-for table_div in main_table:
-    for index,div in enumerate(element_table.find_elements(By.CLASS_NAME,'table-body'),start = 1):
-        values = [item.text for item in div.find_elements(By.CLASS_NAME, 'text')]
-        excel = pd.Dataframe(values)
-        
-        ws.cell(row=index, column=1, value=excel)
-
-wb.save('wrestling_data.xlsx')
+for table_div in main_table.find_elements(By.CLASS_NAME,'table-body'):
+    row = [item.text for item in table_div.find_elements(By.CLASS_NAME,'text')]
+    data.append(row)
+print(data)    
+# wb.save('wrestling_data.xlsx')
