5805926ab104feff2fdbc4126bdf38e3bd503f2a
Kheyransa Pashayeva <xeyransa.pshayeva@gmail.com>
2023-12-13 14:29:16 +0400

    data excel de save edildi

--- scraping.py
+++ scraping.py
@@ -7,7 +7,7 @@ driver = webdriver.Firefox()
 url = 'https://uww.org/events'
 driver.get(url)
 years = ['2018', '2019', '2020', '2021', '2022']
-
+year = []
 data = []
 page_link = []
 # tournament_select = driver.find_element(By.CLASS_NAME, 'waf-select-box')
@@ -18,7 +18,7 @@ element_table = table_wrapper.find_element(By.XPATH,'/html/body/div/div/div/div/
 world_championship = driver.find_element(By.XPATH, '/html/body/div/div/div/div/main/section[4]/section[3]/div/div/div/div/section/div/div/div[2]/div[1]/div/div[2]/div[1]/div[5]')
 world_championship.click()
 select_list = world_championship.find_element(By.CLASS_NAME,'select-list')
-
+count=0
 for li in select_list.find_elements(By.TAG_NAME, 'li'):
     button = li.find_element(By.TAG_NAME, 'button')
     if button.text in years:
@@ -27,13 +27,19 @@ for li in select_list.find_elements(By.TAG_NAME, 'li'):
       for table_div in main_table.find_elements(By.CLASS_NAME,'table-body'):
             row = [item.text for item in table_div.find_elements(By.CLASS_NAME,'text')]
             data.append(row) 
-        # Video linklerin liste yigilmasi   
+        # Video linklerin liste yigilmasi       
       for a in driver.find_elements(By.CLASS_NAME,'table-row'):
             link = a.get_attribute('href')
-            page_link.append(link)
-      time.sleep(2)
-      world_championship.click()
-time.sleep(1)
+            if link == None:
+                continue
+            else:
+                page_link.append(link)
+                year.append(years[count])
+      count+=1    
+    world_championship.click()
+
+              
+
 
 
 # Within the table, find all elements with the class name 'event-title'
@@ -42,17 +48,37 @@ time.sleep(1)
 
 # wb.save('wrestling_data.xlsx')
 # var olan datani tek-tek listlere ayirmaq   
+chunks = []
 def split_list(chunk_size):
-    chunks = []
     for i in range(0,len(data)):
         for j in range(0, len(data[i]), chunk_size):
             chunk = data[i][j:j + chunk_size]
             chunks.append(chunk)
-    print(chunks)
+    return chunks
 
 chunk_size = 5
-split_list(chunk_size) 
-print(page_link)
+# print(page_link)
 
-def tournament_page(url):
-    driver.get(url)
+# def tournament_page(url):
+#     driver.get(url)
+# print(len(page_link),len(year),len(data),len(chunks))
+
+
+def dataset(pl,yr):
+    tournament_name = []
+    location = []
+    type = []
+    age = []
+    style = []
+    data_read = split_list(chunk_size)
+    for data in data_read:
+        tournament_name.append(data[0])
+        location.append(data[1])
+        type.append(data[2])
+        age.append(data[3])
+        style.append(data[4])
+    df = pd.DataFrame({'tournament_name' : tournament_name, 'location' : location, 'type' : type, 'age' : age, 'style' : style, 'page_link' : pl, 'year' : yr})
+    return df
+
+df = dataset(page_link,year)
+df.to_excel('data.xlsx', index = False)
