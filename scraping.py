68cab038a77354b3505b99272fb2cb8b10bc7b31
Elwin <elwinner23@yandex.ru>
2023-12-16 13:06:27 +0400

    update scraping_file

--- scraping.py
+++ scraping.py
@@ -1,50 +1,55 @@
-import os
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
+import time 
 
-os.environ['PATH'] += r'C:\MozilaDrivers'
-years = ['2018', '2019', '2020', '2021', '2022']
 driver = webdriver.Firefox()
-driver.get('https://uww.org/events')
-#tournament_select = driver.find_element(By.CLASS_NAME, 'waf-select-box')
-#tournament_select.click()
-world_championship = driver.find_element(By.XPATH, '//*[@id="b26d198b-4b14-4220-b7bb-481ec09a8f1d"]/div/div/div[2]/div[1]/div/div[2]/div[1]/div[5]/div[1]/p[2][contains(text(), "2023")]')
-world_championship.click()
-select_list = world_championship.find_element(By.XPATH, '//*[@id="b26d198b-4b14-4220-b7bb-481ec09a8f1d"]/div/div/div[2]/div[1]/div/div[2]/div[1]/div[5]/div[2]/ul')
-for li in select_list.find_elements(By.TAG_NAME, 'li'):
-    button = li.find_element(By.TAG_NAME, 'button')
-    if button.text in years:
-        button.click()
-time.sleep(2)
-
+url = 'https://uww.org/events'
+driver.get(url)
+years = ['2018', '2019', '2020', '2021', '2022']
+year = []
+data = []
+page_link = []
+# tournament_select = driver.find_element(By.CLASS_NAME, 'waf-select-box')
+# tournament_select.click()
 main_table = driver.find_element(By.CLASS_NAME,'table-responsive')
 table_wrapper = main_table.find_element(By.CLASS_NAME,'table-wrapper')
 element_table = table_wrapper.find_element(By.XPATH,'/html/body/div/div/div/div/main/section[4]/section[3]/div/div/div/div/section/div/div/div[2]/div[2]/div[1]')
+world_championship = driver.find_element(By.XPATH, '/html/body/div/div/div/div/main/section[4]/section[3]/div/div/div/div/section/div/div/div[2]/div[1]/div/div[2]/div[1]/div[5]')
+world_championship.click()
+select_list = world_championship.find_element(By.CLASS_NAME,'select-list')
+count=0
+for li in select_list.find_elements(By.TAG_NAME, 'li'):
+    button = li.find_element(By.TAG_NAME, 'button')
+    if button.text in years:
+      button.click()
+      # web sehfeden datanin liste elave edilmesi
+      for table_div in main_table.find_elements(By.CLASS_NAME,'table-body'):
+            row = [item.text for item in table_div.find_elements(By.CLASS_NAME,'text')]
+            data.append(row) 
+        # Video linklerin liste yigilmasi       
+      for a in driver.find_elements(By.CLASS_NAME,'table-row'):
+            link = a.get_attribute('href')
+            if link == None:
+                continue
+            else:
+                page_link.append(link)
+                year.append(years[count])
+      count+=1    
+    world_championship.click()
+
+              
+
+
 
 # Within the table, find all elements with the class name 'event-title'
+# wb = Workbook()
+# ws = wb.active
 
-data = []
-video_link = []
-page_link = []
-for table_div in main_table.find_elements(By.CLASS_NAME,'table-body'):
-    row = [item.text for item in table_div.find_elements(By.CLASS_NAME,'text')]
-    data.append(row) 
-for a in driver.find_elements(By.CLASS_NAME,'table-row'):
-    link = a.get_attribute('href')
-    if link == None:
-        continue
-    else:
-        page_link.append(link)
-print(page_link)  
-# elem = driver.find_element(By.CLASS_NAME,'table-body')
-# link = elem[0].get_attribute('href')    
-        
 # wb.save('wrestling_data.xlsx')
-    
+# var olan datani tek-tek listlere ayirmaq   
+chunks = []
 def split_list(chunk_size):
-    chunks = []
     for i in range(0,len(data)):
         for j in range(0, len(data[i]), chunk_size):
             chunk = data[i][j:j + chunk_size]
@@ -52,7 +57,14 @@ def split_list(chunk_size):
     return chunks
 
 chunk_size = 5
-def dataset(pl):
+# print(page_link)
+
+# def tournament_page(url):
+#     driver.get(url)
+# print(len(page_link),len(year),len(data),len(chunks))
+
+
+def dataset(pl,yr):
     tournament_name = []
     location = []
     type = []
@@ -65,10 +77,8 @@ def dataset(pl):
         type.append(data[2])
         age.append(data[3])
         style.append(data[4])
-    df = pd.DataFrame({'tournament_name' : tournament_name, 'location' : location, 'type' : type, 'age' : age, 'style' : style, 'page_link' : pl})
+    df = pd.DataFrame({'tournament_name' : tournament_name, 'location' : location, 'type' : type, 'age' : age, 'style' : style, 'page_link' : pl, 'year' : yr})
     return df
 
-df = dataset(page_link)
-print(len(df['type']))
-print(len(page_link))
-print(df.head())
+df = dataset(page_link,year)
+df.to_excel('data.xlsx', index = False)
